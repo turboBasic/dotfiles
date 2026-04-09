02128d348fd54024d5e7ccfdd911c413f4a1fef6
andriy melnyk <andrii7melnyk@proton.me>
2026-04-10 01:01:42 +0200

    fix: sed command syntax
    Use POSIX sed options instead of GNU sed

--- install.zsh
+++ install.zsh
@@ -119,7 +119,7 @@ function is_sourced() {
 
 function _jq_field() {
     grep '^\s*"'$1'":\s*' \
-    |   sed --regexp-extended '
+    |   sed -E '
             s/^[^:]+:\s*//
             s/,\s*$//
             s/^\s*"//
